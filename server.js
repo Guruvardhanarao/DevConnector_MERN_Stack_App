ad199a89dbdcfad429e5b5ea9c392b82693446d1
PANIDAM <guru-vardhanarao.panidam@capgemini.com>
2024-11-23 19:54:13 +0530

    test routing added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 //DB Config
 const dbUri = require('./config/keys').mongoDBUri;
 
@@ -13,6 +17,12 @@ mongoose
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world'));
+
+// use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const port = process.env.port || 5000;
 
