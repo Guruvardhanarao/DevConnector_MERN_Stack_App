4268856e1ca05b0db7f9a04c0606141de1e5792f
Panidam <guru-vardhanarao.panidam@capgemini.com>
2024-12-11 21:54:29 +0530

    profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const passport = require('passport');
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
 const router = express.Router();
 router.get('/test', (req, res) => res.json({msg: 'test profile...'}));
+
+router.get('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({user:req.user.id})
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = "There is no profile found for this User.";
+                return res.status(404).json(errors);
+            }
+            res.json(profile)
+        })
+        .catch(err => console.log(err));
+})
 
 module.exports = router;
