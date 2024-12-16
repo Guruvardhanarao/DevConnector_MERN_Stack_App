a9b5102184a4df37cdfe4075a03490a9e30bdf33
Panidam <guru-vardhanarao.panidam@capgemini.com>
2024-12-16 11:29:18 +0530

    Post Schema

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create a Post Schema
+const PostSchema = new Schema({
+    user: {
+        type:Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    name: {
+        type:String,
+
+    },
+    avatar: {
+        type:String
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likes:[
+        {
+            user: {
+                type:Schema.Types.ObjectId,
+                ref: 'User'
+            } 
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type:Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            text:{
+                type: String,
+                required: true
+            },
+            name: {
+                type:String
+            },
+            avatar: {
+                type:String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
